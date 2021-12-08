c913f9ad48d983e30cea64fbc49a75e44196d28c
Syntax <88819188+PndaBoi@users.noreply.github.com>
2021-12-08 11:28:46 +0530

    :)
    hope it helps

---
+++ Script.ino
@@ -0,0 +1,22 @@
+//My Traffic Light Simulation With Arduino
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(11, LOW);
+}
